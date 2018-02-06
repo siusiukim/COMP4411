3dff82f26e8b27f48d6aa8cd0ee6dcd87b4452b9
Rex Cheng <hkchengad@connect.ust.hk>
2018-02-06 23:59:11 +0800

    add back lib

---
+++ ImpSkelS17/Brushes.h
@@ -0,0 +1,34 @@
+//
+// PointBrush.h
+//
+// The header file for Point Brush. 
+//
+
+#ifndef BRUSHES_H
+#define BRUSHES_H
+
+#include "ImpBrush.h"
+
+class PointBrush : public ImpBrush
+{
+public:
+	PointBrush(ImpressionistDoc* pDoc = NULL, char* name = NULL);
+
+	void BrushBegin(const Point source, const Point target);
+	void BrushMove(const Point source, const Point target);
+	void BrushEnd(const Point source, const Point target);
+	char* BrushName(void);
+};
+
+class LineBrush : public ImpBrush
+{
+public:
+	LineBrush(ImpressionistDoc* pDoc = NULL, char* name = NULL);
+
+	void BrushBegin(const Point source, const Point target);
+	void BrushMove(const Point source, const Point target);
+	void BrushEnd(const Point source, const Point target);
+	char* BrushName(void);
+};
+
+#endif
--- ImpSkelS17/PointBrush.cpp
+++ ImpSkelS17/PointBrush.cpp
@@ -7,48 +7,48 @@
 
 #include "impressionistDoc.h"
 #include "impressionistUI.h"
-#include "pointbrush.h"
+#include "Brushes.h"
 
 extern float frand();
 
-PointBrush::PointBrush( ImpressionistDoc* pDoc, char* name ) :
-	ImpBrush(pDoc,name)
+PointBrush::PointBrush(ImpressionistDoc* pDoc, char* name) :
+	ImpBrush(pDoc, name)
 {
 }
 
-void PointBrush::BrushBegin( const Point source, const Point target )
+void PointBrush::BrushBegin(const Point source, const Point target)
 {
 	ImpressionistDoc* pDoc = GetDocument();
-	ImpressionistUI* dlg=pDoc->m_pUI;
+	ImpressionistUI* dlg = pDoc->m_pUI;
 
 	int size = pDoc->getSize();
 
 
 
-	glPointSize( (float)size );
+	glPointSize((float)size);
 
-	BrushMove( source, target );
+	BrushMove(source, target);
 }
 
-void PointBrush::BrushMove( const Point source, const Point target )
+void PointBrush::BrushMove(const Point source, const Point target)
 {
 	ImpressionistDoc* pDoc = GetDocument();
-	ImpressionistUI* dlg=pDoc->m_pUI;
+	ImpressionistUI* dlg = pDoc->m_pUI;
 
-	if ( pDoc == NULL ) {
-		printf( "PointBrush::BrushMove  document is NULL\n" );
+	if (pDoc == NULL) {
+		printf("PointBrush::BrushMove  document is NULL\n");
 		return;
 	}
 
-	glBegin( GL_POINTS );
-		SetColor( source );
+	glBegin(GL_POINTS);
+	SetColor(source);
 
-		glVertex2d( target.x, target.y );
+	glVertex2d(target.x, target.y);
 
 	glEnd();
 }
 
-void PointBrush::BrushEnd( const Point source, const Point target )
+void PointBrush::BrushEnd(const Point source, const Point target)
 {
 	// do nothing so far
 }
